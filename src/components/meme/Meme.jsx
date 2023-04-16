2198f0aa0ac8a9d7dd18005d55abc2d7c760cf77
AmmarElsherif2021 <ammar.alshareef2014@gmail.com>
2023-04-16 18:39:09 +0200

    form added

--- src/components/meme/Meme.jsx
+++ src/components/meme/Meme.jsx
@@ -2,8 +2,18 @@ import React from "react";
 import './meme.css'
 export default function Meme(){
     return(
-        <div className='body'>
-          <div className='container'></div>
-        </div>
+        
+          <div className='container'>
+           <form className="form">
+           <input type='text' className="form--input"/>
+           <input type='text' className="form--input"/>
+           <div className='form--btns'>
+           <button className="form--button">add a new line</button>
+           <button className="form--button">get a new meme</button>
+           </div>
+           </form>
+
+          </div>
+        
     )
 }
